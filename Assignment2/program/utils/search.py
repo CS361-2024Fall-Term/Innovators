2ff7292746cebebf3612c5b5e00e8fbe3a587543
reedyai <reedyai@oregonstate.edu>
2024-10-10 18:12:24 -0700

    fix search.py spelling
    spelled category wrong

--- Assignment2/program/utils/search.py
+++ Assignment2/program/utils/search.py
@@ -1,7 +1,7 @@
 class Search:
-    def __init__(self, query, catagory, start_time_range, end_time_range, status, priority):
+    def __init__(self, query, category, start_time_range, end_time_range, status, priority):
         self.query = query
-        self.catagory = catagory
+        self.category = category
         self.start_time_range = start_time_range
         self.end_time_range = end_time_range
         self.status = status
