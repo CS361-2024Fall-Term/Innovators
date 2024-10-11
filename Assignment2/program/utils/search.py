3a8bcaeb9ef0620f671cbc705e5955b892466952
reedyai <reedyai@oregonstate.edu>
2024-10-10 18:08:01 -0700

    Update search.py

--- Assignment2/program/utils/search.py
+++ Assignment2/program/utils/search.py
@@ -1,3 +1,8 @@
 class Search:
-    def __init__(self):
-        pass
+    def __init__(self, query, catagory, start_time_range, end_time_range, status, priority):
+        self.query = query
+        self.catagory = catagory
+        self.start_time_range = start_time_range
+        self.end_time_range = end_time_range
+        self.status = status
+        self.priority = priority
