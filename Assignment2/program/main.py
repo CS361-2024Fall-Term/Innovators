a161239236ad585ba397ffd3b75ee5e8e036cd38
Nadir Isweesi <isweesin@oregonstate.edu>
2024-10-10 17:27:15 -0700

    init classes for the project

---
+++ Assignment2/program/main.py
@@ -0,0 +1,7 @@
+from services.calendar import CalendarService
+from services.notification import NotificationService
+from config.preferences import Preferences
+from utils.search import Search
+from models.events import Events
+from models.task import Task
+from models.user import User
