d9535844fa3138979e2c7e835fb573c548b94a28
terrievu <133167845+terrievu@users.noreply.github.com>
2024-10-10 18:19:13 -0700

    Update daily_overview.py

--- Assignment2/program/services/daily_overview.py
+++ Assignment2/program/services/daily_overview.py
@@ -1,3 +1,5 @@
 class DailyOverview:
-    def __init__(self):
-        pass
+    def __init__(self, events_today, tasks_today, reminders):
+        self.events_today = []
+        self.tasks_today = []
+        self.reminders = []
