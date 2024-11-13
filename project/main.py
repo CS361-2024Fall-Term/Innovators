e9dbeffe0da4241ad23d635daeb0ecb9efc5040b
mortonwi <135153158+mortonwi@users.noreply.github.com>
2024-11-13 15:50:19 -0800

    updated main.py to include both classes

--- project/main.py
+++ project/main.py
@@ -1,1 +1,24 @@
+import tkinter as tk
+from classes.welcomeScreen import WelcomeScreen
+from classes.calendarClass import Cal
 
+# Function that defines the continue function
+def continue_to_calendar():
+    welcome_screen.hide()
+    calendar_app.show()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("EasyCal")
+
+    # Create welcome screen with continue button
+    welcome_screen = WelcomeScreen(root, continue_to_calendar)
+    
+    # Create a calendar object with an empty list of tasks and events
+    calendar_app = Cal(root, 0, 0)
+
+    # Show only the welcome screen
+    calendar_app.hide()
+    welcome_screen.show()
+
+    root.mainloop()
