987de8bec3c25a8c5b9bc696d182355bf63b70f7
willthewa <willthewa@gmail.com>
2024-12-02 22:30:56 -0800

    Updated front image, button, welcome screen

--- src/ui/welcome_screen.py
+++ src/ui/welcome_screen.py
@@ -7,7 +7,7 @@ import sys
 class WelcomeScreen:
     def __init__(self, root, continue_callback):
         self.root = root
-        self.frame = tk.Frame(root)
+        self.frame = tk.Frame(root, bg="white")
         self.frame.pack(fill="both", expand=True)
 
         # Determine the base path depending on the runtime context
@@ -23,26 +23,22 @@ class WelcomeScreen:
         pil_image = Image.open(image_path)
 
         # Resize the image to desired dimensions, e.g., 150x150 pixels
-        pil_image = pil_image.resize((150, 150))
+        pil_image = pil_image.resize((350, 263))
 
         # Convert to Tkinter-compatible image
         self.image = ImageTk.PhotoImage(pil_image)
 
         # Display image in a label, centered at the top
-        image_label = tk.Label(self.frame, image=self.image)
-        image_label.pack(pady=(20, 10)) 
+        image_label = tk.Label(self.frame, image=self.image, bg="white", borderwidth=0, highlightthickness=0)
+        image_label.pack(pady=(40, 10))
 
         # Empty frame for top padding
-        top_padding = tk.Frame(self.frame, height=70)  # Height for top padding
+        top_padding = tk.Frame(self.frame, height=50, bg="white")  # Height for top padding
         top_padding.pack()
-
-        # Welcome message and button
-        welcome_label = tk.Label(self.frame, text="Welcome to EasyCal!", font="Arial 18 bold")
-        welcome_label.pack(pady=20)
 
         continue_button = tk.Button(
             self.frame, text="Start Your Journey", font=("Helvetica", 16, "bold"), command=continue_callback,
-            bg="#1E90FF", fg="white", activebackground="#4682B4", activeforeground="white",
+            bg="#2F39CF", fg="white", activebackground="#010CA5", activeforeground="white",
             relief="flat", borderwidth=5, padx=30, pady=15 
         )
         continue_button.pack(pady=10)
